fff89fca924291e7853e9fe3cc635e55782c9a50
sago0519 <santosh.goudar@netcracker.com>
2020-01-07 16:19:42 +0530

    Springboot fetchers

--- src/main/java/com/netcracker/CoffeeShopApplication/graphql/fetchers/MutationProductFetcher.java
+++ src/main/java/com/netcracker/CoffeeShopApplication/graphql/fetchers/MutationProductFetcher.java
@@ -1,15 +1,20 @@
 package com.netcracker.CoffeeShopApplication.graphql.fetchers;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.netcracker.CoffeeShopApplication.productmanagement.models.Category;
 import com.netcracker.CoffeeShopApplication.productmanagement.models.Product;
+import com.netcracker.CoffeeShopApplication.productmanagement.services.ProductService;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MutationProductFetcher implements DataFetcher<Product> {
+public class MutationProductFetcher implements GraphQLMutationResolver {
+    @Autowired
+    ProductService productService;
 
-    @Override
-    public Product get(DataFetchingEnvironment dataFetchingEnvironment) {
-        return null;
+    public Product createProduct(Product product) {
+        return productService.save(product);
     }
 }
--- src/main/java/com/netcracker/CoffeeShopApplication/graphql/fetchers/ProductsFetcher.java
+++ src/main/java/com/netcracker/CoffeeShopApplication/graphql/fetchers/ProductsFetcher.java
@@ -1,18 +1,28 @@
 package com.netcracker.CoffeeShopApplication.graphql.fetchers;
 
 import java.util.List;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.netcracker.CoffeeShopApplication.productmanagement.models.Product;
+import com.netcracker.CoffeeShopApplication.productmanagement.services.ProductService;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
-public class ProductsFetcher implements DataFetcher<List<Product>> {
+public class ProductsFetcher implements GraphQLQueryResolver {
 
-    @Override
-    public List<Product> get(DataFetchingEnvironment dataFetchingEnvironment) {
-        return null;
+    @Autowired
+    ProductService productService;
+
+    public List<Product> getProducts() {
+        return productService.findAll();
+    }
+
+    public Product getProduct(String productId) throws Exception {
+        return productService.findById(productId);
+
     }
 }
